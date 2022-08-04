7baa453ef4093775205ded1f21562a5024ce317d
Jawad Ahmed <ahm.jawad118@gmail.com>
2022-08-04 11:41:05 +0200

    debug set to false in settings.py for website

--- src/projectazelian/settings.py
+++ src/projectazelian/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hmk*jp@s*6#nl44iynktcnyxnt(!5&zth2zw)4v(b*#j!1=&!*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
