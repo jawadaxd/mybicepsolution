f507ba91f8318302a5fa319e5a5719c6613f002f
Jawad Ahmed <ahm.jawad118@gmail.com>
2022-08-05 11:42:36 +0200

    added new src code mysite with azure configuration suggested in settings.py

--- src/appazelian/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import URLPattern, path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about')
-]
--- src/appazelian/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# Views can be created with functions or classes
-def index(request):
-    return render(request, 'index.html')
-
-def about(request):
-    return render(request, 'about.html')
